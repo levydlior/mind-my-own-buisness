56f25b7a1df0c005110e10a5e0232643f0bd8b72
Lior <levydlior@gmail.com>
2022-08-23 11:19:42 -0600

    add router

--- client/src/firebase.ts
+++ client/src/firebase.ts
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDPhIWtTKrvJGMyK_68TpTf6WhGjhcK8UE",
+  apiKey: `${process.env.REACT_APP_API_KEY}`,
   authDomain: "mind-my-own-buiz-images.firebaseapp.com",
   projectId: "mind-my-own-buiz-images",
   storageBucket: "mind-my-own-buiz-images.appspot.com",
   messagingSenderId: "441578892562",
-  appId: "1:441578892562:web:9fc1d9aec2a4d4c0a3d7cb",
+  appId: `${process.env.REACT_APP_ID}`,
 };
 
 // Initialize Firebase
