c26ba5d814abc791aeea882a4f64b7db381edde7
Lior <levydlior@gmail.com>
2022-12-03 16:57:58 -0500

    change her stack

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,9 +8,5 @@ Business.create(name: 'test buis', user_id: 1)
 Business.create(name: 'third test', user_id: 1)
 Business.create(name: 'dif user', user_id: 2)
 
-Receipt.create(name: "chair", amount: 10, image: "https://image.shutterstock.com/image-vector/realistic-paper-shop-receipt-barcode-600w-768909406.jpg", business_id:1)
-Receipt.create(name: "table", amount: 10, image: "https://image.shutterstock.com/image-vector/realistic-paper-shop-receipt-barcode-600w-768909406.jpg", business_id:1)
-Receipt.create(name: "test",  amount: 10, image: "https://image.shutterstock.com/image-vector/realistic-paper-shop-receipt-barcode-600w-768909406.jpg", business_id:2)
-
 
 puts " done seeding.."
