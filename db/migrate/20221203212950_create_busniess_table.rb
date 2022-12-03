5f7e6108d262f9f0d8369b06eead3f2414c636bb
Lior <levydlior@gmail.com>
2022-12-03 16:34:35 -0500

    fix migration

--- db/migrate/20221203212950_create_busniess_table.rb
+++ db/migrate/20221203212950_create_busniess_table.rb
@@ -1,6 +1,6 @@
 class CreateBusniessTable < ActiveRecord::Migration[7.0]
   def change
-    create_table :busniess_tables do |t|
+    create_table :businesses do |t|
     t.string :name
     t.belongs_to :user, null: false, foreign_key: true
     t.timestamps
